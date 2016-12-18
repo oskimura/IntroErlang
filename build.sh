488c0567c7aacc0560a3f4ab50bed30831ee6172
oskimura <oskimura@gmail.com>
2016-12-18 10:33:36 +0900

    add build rule UlangMake.md

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-pandoc preamble.md introErlang.md Compiler.md  beam.md  ErlangCompile.md  Ulang.md -o IntroductionMakeCompilerWithErlang.tex --bibliography references.bib \
+pandoc preamble.md introErlang.md Compiler.md  beam.md  ErlangCompile.md  Ulang.md UlangMmake.md -o IntroductionMakeCompilerWithErlang.tex --bibliography references.bib \
 && platex manuscript.tex \
 && dvipdfmx manuscript.dvi
